9b2a0765d55ce05ca2cf773bee8f6ae162e04bab
Yugatn <yug.atn@gmail.com>
2026-09-21 11:19:23 +0400

    Expose conversation domain state

--- eugene-messenger/core/domain/src/lib.rs
+++ eugene-messenger/core/domain/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod archive;
 pub mod clock;
+pub mod conversation;
 pub mod errors;
 pub mod favorite;
 pub mod feed;
@@ -11,6 +12,7 @@ pub mod swipe;
 
 pub use archive::ArchiveState;
 pub use clock::{HlcTimestamp, HybridLogicalClock};
+pub use conversation::ConversationState;
 pub use favorite::{FavoriteState, FavoriteStateMachine};
 pub use feed::FeedDirection;
 pub use ids::{DeviceId, MessageId, OperationId};
